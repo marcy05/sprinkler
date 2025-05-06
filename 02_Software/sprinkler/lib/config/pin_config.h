5985b0edbb4e3ce66013b7f2dbdcbe3ccd312858
Marcello <marcello.sgr@gmail.com>
2025-05-06 06:22:39 +0200

    Moved battery pin next to run

--- 02_Software/sprinkler/lib/config/pin_config.h
+++ 02_Software/sprinkler/lib/config/pin_config.h
@@ -6,7 +6,7 @@ namespace Constants {
     constexpr uint8_t DEEP_SLEEP = 32; // It requires a pull-up resistor
 
     constexpr uint8_t LED_RUNNING = 27;
-    constexpr uint8_t LED_BATTERY = 25;
+    constexpr uint8_t LED_BATTERY = 26;
 
     constexpr uint8_t RESET_BUTTON_PIN = 33;
 }
